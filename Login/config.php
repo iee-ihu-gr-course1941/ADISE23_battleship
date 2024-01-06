eda000c81e0e4386e5da1980db61eafa4ad6249a
it113800 <115816306+it113800@users.noreply.github.com>
2024-01-06 03:41:49 +0200

    problem connecting to mysql
    I ve made some attempts to connect to university' s server and all, but I get a 500 error. Also from cmd PHP Fatal error:  Uncaught mysqli_sql_exception: Connection refused. Still haven't figure it out! Lost a day or two trying to find a solution

--- Login/config.php
+++ Login/config.php
@@ -1,15 +1,30 @@
 <?php
 /* Database credentials.*/
-define('DB_SERVER', 'localhost');
+/*define('DB_SERVER', 'users.iee.ihu.gr');
+define('DB_USERNAME', 'it113800');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'battleship3');*/
+/*define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'battleship3');
-
+define('DB_NAME', 'battleship3');*/
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+/*$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);*/
 
 /* Check connection*/
-if($link === false){
+/*if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+}*/
 ?>
+<?php
+$user='it113800';
+$pass= '';
+$host='users.iee.ihu.gr';
+$db = 'battleship3';
+
+
+$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2011/it113800/mysql/run/mysql.sock');
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" .
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>

6df7ec58840a870cc69b23394a082fefa564a055
it113800 <115816306+it113800@users.noreply.github.com>
2024-01-08 01:13:39 +0200

    config correct
    Changed credentials so that it runs on university's server. Also wrote it in a way that only one config document is needed when being on localhost or users with an if -else statement

--- Login/config.php
+++ Login/config.php
@@ -1,30 +1,22 @@
 <?php
 /* Database credentials.*/
-/*define('DB_SERVER', 'users.iee.ihu.gr');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'it113800');
+define('DB_USERNAME_LOCAL', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'battleship3');*/
-/*define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'battleship3');*/
+define('DB_NAME', 'battleship3');
+define('DB_SOCKET','/home/student/it/2011/it113800/mysql/run/mysql.sock');
+
 /* Attempt to connect to MySQL database */
-/*$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);*/
+  if (gethostname()=='users.iee.ihu.gr') {
+         $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, null, DB_SOCKET);
+     } else {
+            $link = mysqli_connect(DB_SERVER, DB_USERNAME_LOCAL, DB_PASSWORD, DB_NAME);
+            }
+
 
 /* Check connection*/
-/*if($link === false){
+if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-}*/
+}
 ?>
-<?php
-$user='it113800';
-$pass= '';
-$host='users.iee.ihu.gr';
-$db = 'battleship3';
-
-
-$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2011/it113800/mysql/run/mysql.sock');
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" .
-    $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}?>
